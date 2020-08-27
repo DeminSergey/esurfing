5ff30c5936ec252b7991dd3ccfdbc41b4c0f63b9
Demin Sergey <econ_invest@mail.ru>
2020-08-27 16:15:32 +1000

    complete raw

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,7 @@
-
+$(document).ready(function(){
+    $('.rent-carousel').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron-circle-left-solid.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-circle-right-solid.svg"></button>',
+    });
+  });
       
